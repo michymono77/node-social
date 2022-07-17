0d714f5ef83539b9f6c73451e4025cbe5f408359
Michiharu <michiharuono77@gmail.com>
2022-07-17 20:11:24 +0900

    user mode working in progress

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+// for creating rest api
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+// mongo connection
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+// middlewares
+
+// Without `express.json()`, `req.body` is undefined.
+app.use(express.json());
+//increase the HTTP header security
+app.use(helmet());
+// log HTTP requests and errors
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+
+app.listen(8800, ()=>{
+  console.log("Backend server is running")
+})
---
+++ routes/users.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+
+router.get("/", (req, res)=>{
+  res.send("This is user routes")
+})
+module.exports = router;
