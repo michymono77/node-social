a4cb32482c65faa62be4f8b74095649dce737245
Michiharu <michiharuono77@gmail.com>
2022-07-17 20:43:02 +0900

    login route finished

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+
 // REGISTER
 router.post("/register", async(req, res) => {
   try {
@@ -21,6 +22,20 @@ router.post("/register", async(req, res) => {
   }
 })
 
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    !validPassword && res.status(400).json("wrong password")
+
+    res.status(200).json(user)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
 
 
 
