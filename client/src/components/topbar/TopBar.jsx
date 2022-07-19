e057ad0b7db4b371b91be17d06ff4218fe32af70
Michiharu <michiharuono77@gmail.com>
2022-07-19 09:02:17 +0900

    navbar created

---
+++ client/src/components/topbar/TopBar.jsx
@@ -0,0 +1,44 @@
+import "./topbar.css"
+import { Search, Person, Chat, Notifications } from "@mui/icons-material"
+export default function TopBar() {
+  return (
+    <div className="topbarContainer">
+      {/* TODO: Logo */}
+      <div className="topbarLeft">
+        <span className="logo">Michisocial</span>
+      </div>
+      {/* TODO: Search */}
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <Search className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      {/* TODO: Links, Icons, Profile image  */}
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="tobarlink">Home</span>
+          <span className="tobarlink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <Person />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <Chat />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <Notifications />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="person" className="topbarImg" />
+      </div>
+    </div>
+  )
+}
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,7 @@
+import Topbar from "../../components/topbar/TopBar";
+
+export default function Home() {
+  return (
+    <Topbar />
+  )
+}
